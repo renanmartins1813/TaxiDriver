1b43c3cc7bda28a86eb5dde4976a3f536f543542
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-21 10:53:03 -0500

    initial express setup

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,1 +1,16 @@
-import express = require('express')
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT || 8080
+
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+})
+
+app.get('/', (req: Request, res: Response) => {
+	res.send("just a response")
+})
