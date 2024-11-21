6721ae873d5e513df80faea391b42333b0ac4f13
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-21 11:10:13 -0500

    setting up server cors package

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from "express"
+import cors from 'cors'
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const app: Express = express()
+app.use(cors())
+
 const port = process.env.PORT || 8080
 
 
