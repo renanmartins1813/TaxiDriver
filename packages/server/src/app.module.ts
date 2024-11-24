139905707b663b5b5a0407693a7caad5d20c33a4
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-24 15:49:59 -0500

    user get all users

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RideModule } from './ride/ride.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, RideModule],
+  imports: [PrismaModule, RideModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
