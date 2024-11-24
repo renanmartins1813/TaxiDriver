a7b7db34d1a6287a95094a503c9526119718c5f2
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-24 15:21:18 -0500

    import PrismaModule

--- packages/server/src/ride/ride.service.ts
+++ packages/server/src/ride/ride.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRideDto } from './dto/create-ride.dto';
 import { UpdateRideDto } from './dto/update-ride.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RideService {
+  constructor(private readonly prismaService: PrismaService) {
+
+  }
   create(createRideDto: CreateRideDto) {
     return 'This action adds a new ride';
   }
