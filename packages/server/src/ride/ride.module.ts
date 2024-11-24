dcb21a9ca48db303a45d626b18eb2a69b38e5392
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-24 15:21:18 -0500

    import PrismaModule

--- packages/server/src/ride/ride.module.ts
+++ packages/server/src/ride/ride.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RideService } from './ride.service';
 import { RideController } from './ride.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RideController],
   providers: [RideService],
 })
-export class RideModule {}
+export class RideModule { }
