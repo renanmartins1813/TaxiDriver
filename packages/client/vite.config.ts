97336821094cd0391af14987437a5af12a8a6b24
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-25 03:42:37 -0500

    yarn add -D vite-plugin-environment and setting it up

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin('all', { prefix: '' })],
   server: { port: 80 }
 })
