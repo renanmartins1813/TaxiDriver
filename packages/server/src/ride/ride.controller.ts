25f040e908245d7ca5490707924aa152241d05c9
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-25 00:49:57 -0500

    working on ride

--- packages/server/src/ride/ride.controller.ts
+++ packages/server/src/ride/ride.controller.ts
@@ -5,30 +5,24 @@ import { UpdateRideDto } from './dto/update-ride.dto';
 
 @Controller('ride')
 export class RideController {
-  constructor(private readonly rideService: RideService) {}
+  constructor(private readonly rideService: RideService) { }
 
-  @Post()
+  @Post('/estimate')
+  estimate() {
+    return this.rideService.estimate()
+  }
+
+  @Post('/confirm')
   create(@Body() createRideDto: CreateRideDto) {
     return this.rideService.create(createRideDto);
   }
 
-  @Get()
-  findAll() {
-    return this.rideService.findAll();
-  }
+  @Get(':userId/:driverId')
+  findOne(@Param('userId') userId: number, @Param('driverId') driverId: number) {
+    if (typeof driverId !== 'undefined') {
+      return `userId: ${userId} and driverId: ${driverId}`
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rideService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRideDto: UpdateRideDto) {
-    return this.rideService.update(+id, updateRideDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rideService.remove(+id);
+    return this.rideService.findOne(+userId);
   }
 }
