9b3bf0a9bcacd52f11a0d51daadcb471d0aae1d2
renanmartins1813 <renanmartins1813@gmail.com>
2024-11-25 17:07:26 -0500

    config css

--- packages/client/src/assets/common/components/Maps.tsx
+++ packages/client/src/assets/common/components/Maps.tsx
@@ -1,14 +1,28 @@
-import React, { useEffect } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useEffect } from "react";
 
 export default function Maps() {
-	useEffect(() => {
-		console.log(process.env.GOOGLE_API_KEY);
+	const shopperLocation = { lat: -23.523580920829797, lng: -46.674265806746206 }
 
+	const { isLoaded } = useJsApiLoader({
+		googleMapsApiKey: String(process.env.GOOGLE_API_KEY)
 	})
 
+	if (!isLoaded) {
+		return (<div>Maps is Loading</div>)
+	}
+
+	// useEffect(() => {
+	// 	console.log(process.env.GOOGLE_API_KEY);
+	//
+	// })
+
 	return (
-		<div>
-			Maps
+		<div className="Maps">
+			Showing Maps
+			<GoogleMap center={shopperLocation}>
+
+			</GoogleMap>
 		</div>
 	)
 }
